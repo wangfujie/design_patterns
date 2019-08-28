c38216c6abc8de6b455942c1e1c2892a990f5d65
wangfj <wangfj@chinawiserv.com>
2019-08-28 17:48:08 +0800

    桥接模式

---
+++ src/structural/bridge/BridgeTest.java
@@ -0,0 +1,14 @@
+package structural.bridge;
+
+/**
+ * 桥接模式：桥接模式就是把事物和其具体实现分开，使它们可以各自独立的变化。桥接的用意是：将抽象化与实现化解耦，使得二者可以独立变化；
+ *  就是类似于数据库中的关联表，将两个维度的类相关联，使一个维度持有另一个维度的成员属性即可（例如电脑：类型维度和品牌维度，电脑持有品牌对象）
+ *
+ * @author wangfj
+ */
+public class BridgeTest {
+
+    public static void main(String[] args) {
+
+    }
+}
